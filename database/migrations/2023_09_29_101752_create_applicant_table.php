f72fbe4d8c7ae63aef78ce55f29c8bf48a46a165
Jade Oloroso <100562473+jdojade@users.noreply.github.com>
2023-09-29 18:18:39 +0800

    Create 2023_09_29_101752_create_applicant_table.php

---
+++ database/migrations/2023_09_29_101752_create_applicant_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('applicants', function (Blueprint $table) {
+            $table->id();
+            $table->string('first_name', 100);
+            $table->string('middle_name', 100);
+            $table->string('last_name', 100);
+            $table->date('birthdate');
+            $table->string('gender', 10);
+            $table->string('cellphone_no', 20);
+            $table->string('address', 150)->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('applicant');
+    }
+};
